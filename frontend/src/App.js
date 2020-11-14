f9ae4c616f24a6a5414fcad57b6c89cb2d7d6cef
phumtutum <victor.popa2000@gmail.com>
2020-11-14 16:59:40 +0000

    added mic

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Example from './Example.js';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Something man</h1>
+      <Example />
+    </div>
+  );
+}
+
+export default App;
